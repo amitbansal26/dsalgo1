cf4695efdb37b6fd6e5bfcb95fa0ee9becb8d4bb
Amit Bansal <amit.bansal@trigyn.com>
2023-10-11 12:28:17 +0530

    added

--- src/main/java/org/example/dp/KnapSack01.java
+++ src/main/java/org/example/dp/KnapSack01.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
  * sum of profits associated with them is the maximum possible.
  */
 public class KnapSack01 {
-
     /**
      * wt[] --> 1 3 4 5
      * val[] -->1 4 5 7
@@ -26,7 +25,7 @@ public class KnapSack01 {
 
     public static void main(String[] args) {
 
-      System.out.println(knapSackmem(wt, val, W, wt.length));
+      System.out.println(knapSackTopDown(wt, val, W, wt.length));
     }
 
    static int knapSack(int wt[], int val[], int W, int n){
@@ -57,14 +56,23 @@ public class KnapSack01 {
     }
 
     static int knapSackTopDown(int wt[], int val[], int W, int n){
-        // Base Condition
-        if (n==0 || W==0){
-            return 0;
+      // Initialize the DP matrix
+        for (int i = 0; i <n+1 ; i++) {
+            for (int j = 0; j <W+1 ; j++) {
+                if (i==0 || j==0){
+                    t[i][j] = 0;
+                }
+            }
         }
-        if (wt[n-1]<=W){
-            return Integer.max(val[n-1]+knapSack(wt, val,W-wt[n-1], n-1), knapSack(wt, val,W, n-1));
-        }else {
-            return knapSack(wt, val,W, n-1);
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < W+1 ; j++) {
+                if (wt[i-1]<=j){
+                    t[i][j] = Integer.max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j] );
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
+      return t[n][W];
     }
 }
