76d7b1663d0c16261f5648e723647adf41eba722
Amit Bansal <amit.bansal@trigyn.com>
2023-10-11 22:06:18 +0530

    added

--- src/main/java/org/example/dp/SubsetSum.java
+++ src/main/java/org/example/dp/SubsetSum.java
@@ -1,13 +1,39 @@
 package org.example.dp;
 
 // if subset is present or not
+
+/**
+ * Given a set of non-negative integers and a value sum,
+ * the task is to check if there is a subset of the given set whose sum is equal to the given sum.
+ * Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+ * Output: True
+ * Explanation: There is a subset (4, 5) with sum 9.
+ *
+ * Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+ * Output: False
+ * Explanation: There is no subset that add up to 30.
+ */
 public class SubsetSum {
 
     public static void main(String[] args) {
-
+      int[] arr = new int[]{3, 34, 4, 12, 5, 2};
+      int sum = 9;
+        System.out.println(subSetSumRecursive(arr, sum, arr.length));
     }
 
 
-
-
+   static boolean subSetSumRecursive(int[] arr, int sum , int n){
+       if(sum==0){
+            return true;
+        }
+       if(n==0){
+           return false;
+       }
+       if ((arr[n-1] <=sum)){
+          return (subSetSumRecursive(arr, sum-arr[n-1], n-1) ||
+                   subSetSumRecursive(arr, sum , n-1));
+       }else{
+           return subSetSumRecursive(arr, sum , n-1);
+       }
+   }
 }
---
+++ src/main/java/org/example/leetcode/LeetCode494.java
@@ -0,0 +1,19 @@
+package org.example.leetcode;
+
+public class LeetCode494 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int findTargetSumWays(int[] nums, int target, int index, int total, int n) {
+        if (index==n){
+            return target == total ? 1 : 0;
+        }
+
+        return findTargetSumWays(nums,target, index+1, total+nums[index], n) +
+        findTargetSumWays(nums,target, index+1, total-nums[index], n);
+    }
+
+
+}
