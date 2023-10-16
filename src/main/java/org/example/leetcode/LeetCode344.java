5425f81665fb5da54ee8671c4138fbd7adb52704
Amit Bansal <amit.bansal@trigyn.com>
2023-10-16 22:34:51 +0530

    added

--- src/main/java/org/example/leetcode/LeetCode344.java
+++ src/main/java/org/example/leetcode/LeetCode344.java
@@ -4,13 +4,14 @@ public class LeetCode344 {
     public static void main(String[] args) {
 
     }
+
     public void reverseString(char[] s) {
-     int l=0;
-     int r= s.length -1;
-        while(l<r){
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
             char ch = s[l];
-            s[l]=s[r];
-            s[r]=ch;
+            s[l] = s[r];
+            s[r] = ch;
             l++;
             r--;
         }
--- src/main/java/org/example/leetcode/LeetCode942.java
+++ src/main/java/org/example/leetcode/LeetCode942.java
@@ -3,10 +3,10 @@ package org.example.leetcode;
 public class LeetCode942 {
 
     public int[] diStringMatch(String s) {
-      int arr[] = new int[s.length() + 1];
-      int l=0,r=s.length();
-        for (int i = 0; i <s.length() ; i++) {
-            arr[i] = s.charAt(i) == 'I' ? l++: r--;
+        int[] arr = new int[s.length() + 1];
+        int l = 0, r = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i) == 'I' ? l++ : r--;
         }
         arr[s.length()] = l;
         return arr;
